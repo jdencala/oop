560c4b1b611f7690965f2a9528ce793ab5fb90ee
jdencala <jdencalahuaya@gmail.com>
2024-08-06 13:23:38 -0500

    Add(VII): Add Authors and Book entities

--- src/main/java/com/ups/oop/bootstrap/BootStrapData.java
+++ src/main/java/com/ups/oop/bootstrap/BootStrapData.java
@@ -1,8 +1,12 @@
 package com.ups.oop.bootstrap;
 
 import com.ups.oop.entity.Animal;
+import com.ups.oop.entity.Author;
+import com.ups.oop.entity.Book;
 import com.ups.oop.entity.Person;
 import com.ups.oop.repository.AnimalRepository;
+import com.ups.oop.repository.AuthorRepository;
+import com.ups.oop.repository.BookRepository;
 import com.ups.oop.repository.PersonRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,10 +15,14 @@ import org.springframework.stereotype.Component;
 public class BootStrapData implements CommandLineRunner {
     private final PersonRepository personRepository;
     private final AnimalRepository animalRepository;
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
 
-    public BootStrapData(PersonRepository personRepository, AnimalRepository animalRepository) {
+    public BootStrapData(PersonRepository personRepository, AnimalRepository animalRepository, AuthorRepository authorRepository, BookRepository bookRepository) {
         this.personRepository = personRepository;
         this.animalRepository = animalRepository;
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
     }
 
     @Override
@@ -62,6 +70,38 @@ public class BootStrapData implements CommandLineRunner {
 
         animalRepository.save(a1);
         animalRepository.save(a2);
+
+        //Books and Authors
+
+        Author au1 = new Author();
+        au1.setName("Alejandro");
+        au1.setLastname("Dumas");
+        authorRepository.save(au1);
+
+        Book b1 = new Book();
+        b1.setTitle("Conde de Montecristo");
+        b1.setEditorial("Pearson");
+        b1.setAuthor(au1);
+        bookRepository.save(b1);
+
+        au1.getBooks().add(b1);
+        authorRepository.save(au1);
+
+        Author au2 = new Author();
+        au2.setName("J.K.");
+        au2.setLastname("Rowling");
+        authorRepository.save(au2);
+
+        Book b2 = new Book();
+        b2.setTitle("Harry Potter");
+        b2.setEditorial("Us Editorial");
+        b2.setAuthor(au2);
+        bookRepository.save(b2);
+
+        au1.getBooks().add(b2);
+        authorRepository.save(au2);
+
+
 
         System.out.println("--------- Started BootstrapData -------------");
         System.out.println("Number of Persons: " + personRepository.count());
---
+++ src/main/java/com/ups/oop/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.ups.oop.repository;
+
+import com.ups.oop.entity.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository  extends CrudRepository<Author, Long> {
+}
